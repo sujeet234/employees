725dd3efc3a52a0382c8abe3817df78c01896102
sujeet_1miles <sujeet74560@gmail.com>
2023-09-23 18:38:48 +0530

    employees

--- empServer.js
+++ empServer.js
@@ -16,7 +16,7 @@ app.use(function(req,res,next){
     next();
 });
 //process.env.PORT ||
-const port =  2410;
+const port = process.env.PORT || 2410;
 app.listen(port,()=>console.log(`Listening on port ${port}`));
 
 let {getConnection} = require("./empDB.js");
